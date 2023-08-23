652bc5d72201c1c0c5c7ffb0acc7973be2dff836
Kevin <staffordkevinjames@gmail.com>
2023-08-23 16:53:15 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kevin Stafford",
+    streetAddress: "20 Main St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee};
+    delete newObj.name;
+    
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+
+    return employee;
+};
